a763e62320a7bfd2f492381e14c81e812aa041b9
Brian Wagner <biwagner@usc.edu>
2025-06-22 14:45:36 -0700

    Added sign out button and improved selection animation by adding confetti and celebratory sound

--- lib/frontend/pages/decision_making/categories/selectionPage.dart
+++ lib/frontend/pages/decision_making/categories/selectionPage.dart
@@ -1,20 +1,33 @@
 import 'dart:async';
+import 'dart:math';
+import 'package:audioplayers/audioplayers.dart';
 import 'package:blink/frontend/pages/decision_making/category_selection.dart';
+import 'package:confetti/confetti.dart';
 import 'package:flutter/material.dart';
 
 class SelectionPage extends StatefulWidget {
-  final String restaurantName;
+  final String selectionName;
 
-  const SelectionPage(this.restaurantName, {super.key});
+  const SelectionPage(this.selectionName, {super.key});
 
   @override
   _SelectionPageState createState() => _SelectionPageState();
 }
 
 class _SelectionPageState extends State<SelectionPage> {
+  late ConfettiController _confettiController;
+  
   @override
   void initState() {
     super.initState();
+
+    _confettiController =
+        ConfettiController(duration: const Duration(seconds: 2));
+    _confettiController.play();
+
+    final player = AudioPlayer();
+    player.play(AssetSource('sounds/orchestra-triumphant-trumpets-2285.wav'));
+
     Timer(
       const Duration(seconds: 5),
       () {
@@ -42,41 +55,123 @@ class _SelectionPageState extends State<SelectionPage> {
   }
 
   @override
+  void dispose() {
+    _confettiController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return PopScope(
       canPop: false,
       child: Scaffold(
-        body: Center(
-          child: Padding(
-            padding: const EdgeInsets.only(left: 16, right: 16),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  widget.restaurantName,
-                  style: const TextStyle(
-                    fontSize: 48,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,
-                  ),
-                  textAlign: TextAlign.center,
-                ),
-                const SizedBox(height: 20),
-                const Text(
-                  "Decision made. We're proud of you.",
-                  style: TextStyle(
-                    fontSize: 18,
-                    color: Colors.black,
-                    fontFamily: 'OpenSans',
-                  ),
-                  textAlign: TextAlign.center,
-                ),
-              ],
+        body: Stack(
+          alignment: Alignment.center,
+          children: [
+            Align(
+              alignment: Alignment.topCenter,
+              child: ConfettiWidget(
+                confettiController: _confettiController,
+                blastDirectionality: BlastDirectionality.explosive,
+                shouldLoop: false,
+                numberOfParticles: 40,
+                maxBlastForce: 30,
+                minBlastForce: 10,
+                emissionFrequency: .05,
+                gravity: 0.2,
+                colors: [
+                  Color.fromARGB(255, 72, 103, 131), 
+                  Color.fromARGB(255, 235, 250, 250), 
+                  Color.fromARGB(255, 109, 131, 242), 
+                  Color.fromARGB(255, 134, 217, 247), 
+                  Color.fromARGB(255, 145, 166, 166), 
+                  Color.fromARGB(255, 140, 157, 157), 
+                ],
+                maximumSize: Size(15, 15),
+                minimumSize: Size(15, 15),
+                createParticlePath: (size) {
+                  final shapePicker = Random().nextInt(3);
+                  switch (shapePicker) {
+                    case 0:
+                      return drawStar(size);
+                    case 1:
+                      return drawTriangle(size);
+                    case 2:
+                    default:
+                      return drawCircle(size);
+                  }
+                }
+              ),
             ),
-          ),
+            Center(
+              child: Padding(
+                padding: const EdgeInsets.only(left: 16, right: 16),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      widget.selectionName,
+                      style: const TextStyle(
+                        fontSize: 48,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                    const SizedBox(height: 20),
+                    const Text(
+                      "Decision made. We're proud of you.",
+                      style: TextStyle(
+                        fontSize: 18,
+                        color: Colors.black,
+                        fontFamily: 'OpenSans',
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ]
         ),
         backgroundColor: const Color.fromARGB(255, 183, 236, 236),
       ),
     );
   }
+}
+
+Path drawStar(Size size) {
+  // Star shape
+  const int numPoints = 5;
+  final Path path = Path();
+  final double halfWidth = size.width / 2;
+  final double externalRadius = halfWidth;
+  final double internalRadius = halfWidth / 2.5;
+  final angle = pi / numPoints;
+
+  for (int i = 0; i < numPoints * 2; i++) {
+    final isEven = i % 2 == 0;
+    final radius = isEven ? externalRadius : internalRadius;
+    final x = halfWidth + radius * cos(i * angle);
+    final y = halfWidth + radius * sin(i * angle);
+    if (i == 0) {
+      path.moveTo(x, y);
+    } else {
+      path.lineTo(x, y);
+    }
+  }
+  path.close();
+  return path;
+}
+
+Path drawTriangle(Size size) {
+  return Path()
+    ..moveTo(size.width / 2, 0)
+    ..lineTo(size.width, size.height)
+    ..lineTo(0, size.height)
+    ..close();
+}
+
+Path drawCircle(Size size) {
+  return Path()..addOval(Rect.fromCircle(center: Offset(size.width / 2, size.height / 2), radius: size.width / 2));
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,6 +6,7 @@ import FlutterMacOS
 import Foundation
 
 import app_links
+import audioplayers_darwin
 import geolocator_apple
 import google_sign_in_ios
 import location
@@ -17,6 +18,7 @@ import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AppLinksMacosPlugin.register(with: registry.registrar(forPlugin: "AppLinksMacosPlugin"))
+  AudioplayersDarwinPlugin.register(with: registry.registrar(forPlugin: "AudioplayersDarwinPlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
   LocationPlugin.register(with: registry.registrar(forPlugin: "LocationPlugin"))
